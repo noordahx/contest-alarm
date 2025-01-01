67c6d9c8a2142729a656790b4457215c05afc4cd
Nurda <taumergenov@protonmail.com>
2025-01-01 23:52:02 +0800

    fix codeforces api

--- src/main/scala/AppMain.scala
+++ src/main/scala/AppMain.scala
@@ -1,84 +1,21 @@
 package app
 
 import apis._
-// import com.github.cdimascio.dotenv.Dotenv
 
-// import java.util.Properties
-// import javax.mail._
-// import javax.mail.internet._
 
 object AppMain extends App {
 
-  // val dotenv = Dotenv.load()
+  case class Contest(
+    id: Option[Long],
+    name: String,
+    startDateTime: String,
+    duration: String,
+    link: Option[String]
+  )
 
-  // val senderEmail     = dotenv.get("SENDER_EMAIL")
-  // val senderPassword  = dotenv.get("SENDER_PASSWORD")
-  // val recipientEmail  = dotenv.get("RECIPIENT_EMAIL")
-  // val smtpHost        = dotenv.get("SMTP_HOST")
-  // val smtpPort        = dotenv.get("SMTP_PORT")
 
-
-
-  val codeforcesContests = CodeforcesApi.fetchUpcomingContests()
-  // val AtcoderContests = AtcoderApi.fetchUpcomingContests()
-  // val leetcodeContests = LeetcodeApi.fetchUpcomingContests()
+  val codeforcesContests: List[Contest] = CodeforcesApi.fetchUpcomingContests()
   
   println(s"Upcoming Codeforces contests: $codeforcesContests")
-  // println(s"Upcoming Atcoder contests: $AtcoderContests")
-  // val contestsMessage = createContestsMessage(cfContests, AtcoderContests)
 
-  // if (contestsMessage.nonEmpty) {
-  //   sendEmail(
-  //     host        = smtpHost,
-  //     port        = smtpPort,
-  //     senderEmail = senderEmail,
-  //     senderPass  = senderPassword,
-  //     recipient   = recipientEmail,
-  //     subject     = "Upcoming Programming Contests",
-  //     messageBody = contestsMessage
-  //   )
-  //   println("Email with contests info sent successfully")
-  // } else {
-  //   println("No upcoming contests found")
-  // }
-
-  def createContestsMessage(
-    cfContests: List[CodeforcesApi.CodeforcesContest],
-    AtcoderContests: List[AtcoderApi.AtcoderContest]
-  ): String = {
-    val sb = new StringBuilder
-    
-    sb.append("=== Codeforces Contests ===\n")
-    if (cfContests.isEmpty) {
-      sb.append("No upcoming contests")
-    } else {
-      cfContests.foreach { c =>
-        sb.append(s"* ${c.name} (ID: ${c.id}, starts at ${c.startTimeSeconds}, duration ${c.durationSeconds}\n")
-      }
-    }
-
-    sb.append("\n=== Atcoder Contests ===\n")
-    if (AtcoderContests.isEmpty) {
-      sb.append("No upcoming contests")
-    } else {
-      AtcoderContests.foreach { c =>
-        sb.append(s"* ${c.title} (starts at ${c.start_time}, ends at ${c.end_time})\n")
-      }
-    }
-    
-    sb.toString()
-  }
-  
-
-  // def sendEmail(
-  //   host: String,
-  //   port: String,
-  //   senderEmail: String,
-  //   senderPass: String,
-  //   recipient: String,
-  //   subject: String,
-  //   messageBody: String
-  // ): Unit = {
-    
-  // }
 }
--- src/main/scala/apis/CodeforcesApi.scala
+++ src/main/scala/apis/CodeforcesApi.scala
@@ -1,9 +1,12 @@
 package apis
 
+import app.AppMain.Contest
 import sttp.client3._
 import sttp.client3.circe._
 import io.circe._
 import io.circe.generic.semiauto._
+import java.text.SimpleDateFormat
+import java.util.Date
 
 object CodeforcesApi {
   case class CodeforcesContest(
@@ -17,28 +20,44 @@ object CodeforcesApi {
 
   private val CodeforcesEndpoint = uri"https://codeforces.com/api/contest.list"
 
-  def fetchUpcomingContests(): List[CodeforcesContest] = {
+  def fetchUpcomingContests(): List[Contest] = {
     val backend = HttpURLConnectionBackend()
     val request = basicRequest
       .get(CodeforcesEndpoint)
       .response(asJson[Json])
-
+    
     val response = request.send(backend)
 
     response.body match {
       case Right(json) =>
         val resultCursor = json.hcursor.downField("result")
         resultCursor.as[List[CodeforcesContest]] match {
-          case Right(contests) =>
-            val currentTime = System.currentTimeMillis() / 1000
-            contests.filter(_.startTimeSeconds > currentTime)
-          case Left(err) =>
-            println(s"Failed to decode Codeforces contests: $err")
-            List.empty
+          case Right(codeforcesContests) =>
+            val currentTimeSec = System.currentTimeMillis() / 1000
+
+            codeforcesContests
+              .filter(_.startTimeSeconds > currentTimeSec)
+              .map { c => 
+                // 1. Convert startTimeSecods -> Date
+                val date = new Date(c.startTimeSeconds * 1000L)
+                val dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                val startDateTime = dateFormatter.format(date)
+
+                // 2. Convert durationSeconds -> "X hr Y min"
+                val hours = c.durationSeconds / 3600
+                val minutes = (c.durationSeconds % 3600) / 60
+                val durationStr = s"$hours hr $minutes min"
+
+                // 3. Final Contest object
+                Contest(
+                  id = Some(c.id),
+                  name = c.name,
+                  startDateTime = startDateTime,
+                  duration = durationStr,
+                  link = Some(s"https://codeforces.com/contest/${c.id}")
+                )
+              }
         }
-      case Left(error) =>
-        println(s"Failed to fetch Codeforces data: $error")
-        List.empty
     }
   }
 }
